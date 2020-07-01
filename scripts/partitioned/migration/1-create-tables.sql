1f2860f6f38aeae1e3903d5ea5fbdf4e09979a9c
Sławomir Kowalski <kowalskislawomirkarol@gmail.com>
2020-07-01 10:36:09 +0200

    change schema to reduce size of single row (#39)

--- scripts/partitioned/migration/1-create-tables.sql
+++ scripts/partitioned/migration/1-create-tables.sql
@@ -1,16 +1,16 @@
 CREATE TABLE public.journal_partitioned
 (
     ordering        BIGSERIAL,
-    persistence_id  TEXT                  NOT NULL,
     sequence_number BIGINT                NOT NULL,
     deleted         BOOLEAN DEFAULT FALSE NOT NULL,
-    tags            int[],
+    persistence_id  TEXT                  NOT NULL,
     message         BYTEA                 NOT NULL,
+    tags            int[],
     PRIMARY KEY (persistence_id, sequence_number)
 ) PARTITION BY LIST (persistence_id);
 
 CREATE INDEX journal_partitioned_ordering_idx ON public.journal_partitioned USING BRIN (ordering);
-CREATE INDEX journal_partitioned_tags_idx ON public.journal USING GIN(tags);
+CREATE INDEX journal_partitioned_tags_idx ON public.journal USING GIN (tags);
 
 CREATE TABLE public.event_tag
 (
