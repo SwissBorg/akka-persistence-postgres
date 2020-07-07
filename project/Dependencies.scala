1112e3537bc889a7c79d6881232f726577ea6f75
slawomir <kowalskislawomirkarol@gmail.com>
2020-07-07 11:31:10 +0200

    remove migration subproject #56

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,4 @@
 import sbt._
-import Keys._
 
 object Dependencies {
   val Nightly = sys.env.get("TRAVIS_EVENT_TYPE").contains("cron")
@@ -18,8 +17,7 @@ object Dependencies {
 
   val ScaffeineVersion = "4.0.1"
 
-  val JdbcDrivers = Seq(
-    "org.postgresql" % "postgresql" % "42.2.12")
+  val JdbcDrivers = Seq("org.postgresql" % "postgresql" % "42.2.12")
 
   val Libraries: Seq[ModuleID] = Seq(
       "com.typesafe.akka" %% "akka-persistence-query" % AkkaVersion,
@@ -33,11 +31,4 @@ object Dependencies {
       "com.typesafe.akka" %% "akka-stream-testkit" % AkkaVersion % Test,
       "com.typesafe.akka" %% "akka-testkit" % AkkaVersion % Test,
       "org.scalatest" %% "scalatest" % ScalaTestVersion % Test) ++ JdbcDrivers.map(_ % Test)
-
-  val Migration: Seq[ModuleID] = Seq(
-      "org.flywaydb" % "flyway-core" % "6.4.2",
-      "com.typesafe" % "config" % "1.4.0",
-      "ch.qos.logback" % "logback-classic" % "1.2.3",
-      "org.testcontainers" % "postgresql" % "1.14.2" % Test,
-      "org.scalatest" %% "scalatest" % ScalaTestVersion % Test) ++ JdbcDrivers.map(_ % Provided)
 }
