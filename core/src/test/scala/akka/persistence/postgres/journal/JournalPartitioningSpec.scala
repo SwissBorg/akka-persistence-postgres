5fbb78f5db09d58881d9f0e88fb3bb1d78c173ca
Marcin Kubala <marcin@mkubala.pl>
2020-08-28 14:07:12 +0200

    Remove unnecessary copyright

--- core/src/test/scala/akka/persistence/postgres/journal/JournalPartitioningSpec.scala
+++ core/src/test/scala/akka/persistence/postgres/journal/JournalPartitioningSpec.scala
@@ -1,8 +1,3 @@
-/*
- * Copyright (C) 2014 - 2019 Dennis Vriend <https://github.com/dnvriend>
- * Copyright (C) 2019 - 2020 Lightbend Inc. <https://www.lightbend.com>
- */
-
 package akka.persistence.postgres.journal
 
 import java.util.UUID
