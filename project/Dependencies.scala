ecef4926c8ee981b4dedcad6722bb06305052e7c
Jonas Chapuis <jonas.chapuis@zf.com>
2021-12-24 08:42:27 +0100

    Mark akka-persistence-query as `Provided`
    So that dependent projects do not need to exclude akka deps or align versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,7 @@ object Dependencies {
   val Scala213 = "2.13.7"
   val ScalaVersions = Seq(Scala213)
 
-  val AkkaVersion = "2.6.18"
-  val AkkaBinaryVersion = "2.6"
+  val AkkaVersion = "2.6.16"
   val FlywayVersion = "8.2.3"
   val ScaffeineVersion = "5.1.1"
   val ScalaTestVersion = "3.2.10"
@@ -25,16 +24,17 @@ object Dependencies {
     "com.typesafe.akka" %% "akka-persistence-tck" % AkkaVersion % Test,
     "com.typesafe.akka" %% "akka-stream-testkit" % AkkaVersion % Test,
     "com.typesafe.akka" %% "akka-testkit" % AkkaVersion % Test,
-    "com.typesafe.akka" %% "akka-persistence-query" % AkkaVersion,
+    "com.typesafe.akka" %% "akka-persistence-query" % AkkaVersion % Provided,
     "com.typesafe.slick" %% "slick" % SlickVersion,
     "com.typesafe.slick" %% "slick-hikaricp" % SlickVersion,
     "org.scalatest" %% "scalatest" % ScalaTestVersion % Test) ++ JdbcDrivers.map(_ % Test)
 
-  val Migration: Seq[ModuleID] = (Seq(
-    "org.scalatest" %% "scalatest" % ScalaTestVersion,
-    "com.typesafe.akka" %% "akka-testkit" % AkkaVersion,
-    "ch.qos.logback" % "logback-classic" % LogbackVersion,
-    "com.typesafe.akka" %% "akka-slf4j" % AkkaVersion,
-    "com.typesafe.akka" %% "akka-serialization-jackson" % AkkaVersion,
-    "org.flywaydb" % "flyway-core" % FlywayVersion) ++ JdbcDrivers).map(_ % Test)
+  val Migration: Seq[ModuleID] =
+    Seq("com.typesafe.akka" %% "akka-persistence-query" % AkkaVersion).map(_ % Compile) ++ (Seq(
+      "org.scalatest" %% "scalatest" % ScalaTestVersion,
+      "com.typesafe.akka" %% "akka-testkit" % AkkaVersion,
+      "ch.qos.logback" % "logback-classic" % LogbackVersion,
+      "com.typesafe.akka" %% "akka-slf4j" % AkkaVersion,
+      "com.typesafe.akka" %% "akka-serialization-jackson" % AkkaVersion,
+      "org.flywaydb" % "flyway-core" % FlywayVersion) ++ JdbcDrivers).map(_ % Test)
 }
