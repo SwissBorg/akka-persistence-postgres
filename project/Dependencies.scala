4c5ee76e53dd183681bb27c8f28dfa41361df72d
Scala Steward <me@scala-steward.org>
2022-01-24 07:29:03 +0100

    Update scalatest to 3.2.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val AkkaVersion = "2.6.16"
   val FlywayVersion = "8.3.0"
   val ScaffeineVersion = "5.1.2"
-  val ScalaTestVersion = "3.2.10"
+  val ScalaTestVersion = "3.2.11"
   val SlickVersion = "3.3.3"
   val SlickPgVersion = "0.20.2"
 
