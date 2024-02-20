533633aa78445f41f847033581c3ddee528be913
Scala Steward <scala_steward@virtuslab.com>
2024-02-20 09:40:58 +0000

    Update scalatest to 3.2.18

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val AkkaVersion = "2.6.16"
   val FlywayVersion = "9.20.0"
   val ScaffeineVersion = "5.2.1"
-  val ScalaTestVersion = "3.2.17"
+  val ScalaTestVersion = "3.2.18"
   val SlickVersion = "3.4.1"
   val SlickPgVersion = "0.21.1"
   val SslConfigVersion = "0.6.1"
