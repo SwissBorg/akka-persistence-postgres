283d9552705de5e24bec9228d47bc7acd11f4c66
Scala Steward <scala_steward@virtuslab.com>
2024-03-14 20:55:18 +0000

    Update postgresql to 42.7.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
 
   val LogbackVersion = "1.4.14"
 
-  val JdbcDrivers = Seq("org.postgresql" % "postgresql" % "42.7.1")
+  val JdbcDrivers = Seq("org.postgresql" % "postgresql" % "42.7.3")
 
   val Libraries: Seq[ModuleID] = Seq(
     "ch.qos.logback" % "logback-classic" % LogbackVersion % Test,
